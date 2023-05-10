28eaaa4506dcbe9dbb44cdae9862d8c98f49aaa7
vasilkovalyov <vasilkovalyov@gmail.com>
2023-05-10 18:00:19 +0300

    client: created middleware for avoid access to private pages

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
 import pages from '@/constants/pages'
+import { isAuth } from './services/auth'
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')
   const pathname = req.nextUrl.pathname.split('/')[1]
-  if (pathname === pages.admin.replace('/', '') && !token) {
+  const authToken = req.cookies.get('token')
+
+  if (pathname === 'admin' && !authToken) {
+    return NextResponse.redirect(new URL('/404', req.url))
+  }
+
+  const response = await isAuth(authToken?.value)
+  if (pathname === pages.admin.replace('/', '') && !response.isAuth) {
     return NextResponse.redirect(new URL('/404', req.url))
   }
 
