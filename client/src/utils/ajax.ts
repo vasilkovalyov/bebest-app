4cfe483e2c265ff129747373b94a5b078344af6e
vasilkovalyov <vasilkovalyov@gmail.com>
2023-05-10 17:59:35 +0300

    client: refactored ajax config, updated the fonts

--- client/src/utils/ajax.ts
+++ client/src/utils/ajax.ts
@@ -1,15 +1,29 @@
 import axios from 'axios'
 import getConfig from 'next/config'
 
-function $api(token?: string) {
-  const { publicRuntimeConfig } = getConfig()
+function $api(token?: string | undefined) {
+  let authToken: string = ''
+  if (token) {
+    authToken = token
+  } else {
+    authToken = ''
+  }
+
+  let apiUrl = ''
+  if (getConfig()) {
+    const { publicRuntimeConfig } = getConfig()
+    apiUrl = publicRuntimeConfig.apiUrl
+  } else {
+    apiUrl = process.env.API_URL || ''
+  }
+
   return axios.create({
-    baseURL: `${publicRuntimeConfig.apiUrl}/api/`,
+    baseURL: `${apiUrl}/api/`,
     withCredentials: true,
     method: 'get, post, put, delete',
     headers: {
       'X-Requested-With': 'XMLHttpRequest',
-      Authorization: token || '',
+      Authorization: authToken,
     },
   })
 }
