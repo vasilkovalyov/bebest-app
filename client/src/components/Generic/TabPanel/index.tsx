9c51ad04ceb14d8022209ac7e0742d528fab8196
vasilkovalyov <vasilkovalyov@gmail.com>
2023-05-11 19:40:08 +0300

    client: created tabPanel component

---
+++ client/src/components/Generic/TabPanel/index.tsx
@@ -0,0 +1,26 @@
+// libs
+import React from 'react'
+
+// material ui components
+import Box from '@mui/material/Box'
+
+// relate utils
+import { TabPanelProps } from './TabPanel.type'
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box>{children}</Box>}
+    </div>
+  )
+}
+
+export default TabPanel
