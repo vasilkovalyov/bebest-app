8a379005ec96a0eb6cf50bf96f694633e89cfe56
vasilkovalyov <vasilkovalyov@gmail.com>
2023-05-24 21:35:18 +0300

    client: refresh account student component

--- client/src/components/Forms/Account/AccountStudent/AccountStudent.type.ts
+++
@@ -1,8 +0,0 @@
-import { UserAccountInfoEditType } from '@/services/student'
-
-export interface IAccountStudentFormProps {
-  initialData: UserAccountInfoEditType
-  onSubmit: (props: UserAccountInfoEditType) => void
-  isLoading: boolean
-  validationMessage?: string | null
-}
--- client/src/components/Forms/Account/AccountStudent/index.tsx
+++ client/src/components/Forms/Account/AccountStudent/index.tsx
@@ -6,16 +6,11 @@ import { yupResolver } from '@hookform/resolvers/yup'
 //redux
 import { useAppSelector, useActions } from '@/redux/hooks'
 
-//hooks
-import { useNotification } from '@/hooks/useNotification'
-
 // material ui components
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import CircularProgress from '@mui/material/CircularProgress'
-import Snackbar from '@mui/material/Snackbar'
-import Alert from '@mui/material/Alert'
 
 // relate utils
 import { AccountStudentFormValidationSchema } from './AccountStudent.validation'
@@ -24,18 +19,40 @@ import { AccountStudentFormValidationSchema } from './AccountStudent.validation'
 import studentService, { UserAccountInfoEditType } from '@/services/student'
 import { AxiosError } from 'axios'
 
-function AccountStudentForm() {
+const fields: Readonly<
+  {
+    name: keyof UserAccountInfoEditType
+    label: string
+    textarea?: boolean
+  }[]
+> = [
+  {
+    name: 'name',
+    label: 'Name',
+  },
+  {
+    name: 'surname',
+    label: 'Surname',
+  },
+  {
+    name: 'email',
+    label: 'Email',
+  },
+  {
+    name: 'phone',
+    label: 'Phone',
+  },
+  {
+    name: 'about',
+    label: 'About',
+    textarea: true,
+  },
+]
+
+function AccountStudentForm({ onHandleClose }: { onHandleClose: () => void }) {
   const { setAuthState } = useActions()
   const user = useAppSelector((state) => state.user.user)
   const [isLoading, setIsLoading] = useState<boolean>(false)
-
-  const [
-    isVisibleNotification,
-    messageNotification,
-    showNotification,
-    closeNotification,
-    setMessageNotification,
-  ] = useNotification()
 
   const {
     handleSubmit,
@@ -63,8 +80,7 @@ function AccountStudentForm() {
         props
       )
       setAuthState(response.data)
-      showNotification()
-      setMessageNotification('Account info has updated')
+      onHandleClose()
     } catch (e) {
       if (e instanceof AxiosError) {
         console.log(e.message)
@@ -81,98 +97,39 @@ function AccountStudentForm() {
         onSubmit={handleSubmit(onSubmit)}
         className="form form-login"
       >
-        <Box marginBottom={2}>
-          <TextField
-            {...register('name')}
-            id="name"
-            name="name"
-            type="name"
-            label="Name"
-            variant="standard"
-            className="form-field"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            error={!!errors.name?.message}
-            helperText={errors.name?.message}
-          />
-        </Box>
-        <Box marginBottom={2}>
-          <TextField
-            {...register('surname')}
-            id="surname"
-            name="surname"
-            type="surname"
-            label="Surname"
-            variant="standard"
-            className="form-field"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            error={!!errors.surname?.message}
-            helperText={errors.surname?.message}
-          />
-        </Box>
-        <Box marginBottom={2}>
-          <TextField
-            {...register('email')}
-            id="email"
-            name="email"
-            type="email"
-            label="Email"
-            variant="standard"
-            className="form-field"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            error={!!errors.email?.message}
-            helperText={errors.email?.message}
-          />
-        </Box>
-        <Box marginBottom={2}>
-          <TextField
-            {...register('phone')}
-            id="phone"
-            name="phone"
-            type="text"
-            label="Phone"
-            variant="standard"
-            className="form-field"
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            error={!!errors.phone?.message}
-            helperText={errors.phone?.message}
-          />
-        </Box>
-        <Box marginBottom={2}>
-          <TextField
-            {...register('about')}
-            id="about"
-            name="about"
-            type="text"
-            label="About"
-            variant="standard"
-            className="form-field"
-            fullWidth
-            multiline
-            minRows={5}
-            InputLabelProps={{ shrink: true }}
-            error={!!errors.about?.message}
-            helperText={errors.about?.message}
-          />
-        </Box>
+        {fields.map(({ name, label, textarea }) => (
+          <Box key={name} marginBottom={2}>
+            <TextField
+              {...register(name)}
+              id={name}
+              name={name}
+              type="text"
+              label={label}
+              variant="standard"
+              className="form-field"
+              fullWidth
+              {...(textarea && { multiline: true, minRows: 5 })}
+              InputLabelProps={{ shrink: true }}
+              error={!!errors[name]?.message}
+              helperText={errors[name]?.message}
+            />
+          </Box>
+        ))}
         <Box display="flex" alignItems="center" marginBottom={3}>
-          <Button type="submit" variant="contained" disabled={isLoading}>
+          <Button
+            type="submit"
+            variant="contained"
+            size="small"
+            disabled={isLoading}
+          >
             Save
           </Button>
           <Box ml={2}>{isLoading ? <CircularProgress size={16} /> : null}</Box>
+          <Button variant="outlined" size="small" onClick={onHandleClose}>
+            Close
+          </Button>
         </Box>
       </form>
-      <Snackbar
-        open={isVisibleNotification}
-        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-        autoHideDuration={3000}
-        onClose={closeNotification}
-      >
-        <Alert severity="success">{messageNotification}</Alert>
-      </Snackbar>
     </Box>
   )
 }
