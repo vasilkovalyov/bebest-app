baa52f66d450813839f914698ce24cbb7747d74d
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-05 10:53:20 +0300

    client: create hook for getting user info optionally for teacher or student

---
+++ client/src/hooks/useLoadUserInfo.ts
@@ -0,0 +1,25 @@
+//redux
+import { useActions } from '@/redux/hooks'
+
+import teacherService from '@/services/teacher'
+import studentService from '@/services/student'
+import { UserRole } from '@/types/role'
+
+export function useLoadUserInfo() {
+  const { setAuthState } = useActions()
+
+  async function loadUserInfo(userRole: UserRole) {
+    if (userRole === 'teacher') {
+      const teachernfoResponse = await teacherService.getUserInfo()
+      setAuthState(teachernfoResponse.data)
+    }
+    if (userRole === 'student') {
+      const studentInfoResponse = await studentService.getUserInfo()
+      setAuthState(studentInfoResponse.data)
+    }
+  }
+
+  return {
+    loadUserInfo,
+  }
+}
