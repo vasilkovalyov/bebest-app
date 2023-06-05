382117cc06fd77ae9f6af94699add7f28e361b01
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-05 17:37:48 +0300

    client: create upload file service

---
+++ client/src/services/upload-file.ts
@@ -0,0 +1,24 @@
+import $api from '@/utils/ajax'
+import { AxiosResponse } from 'axios'
+import { UserRole } from '@/types/role'
+import { PRIVATE_REQUESTS } from '@/constants/api-requests'
+
+class UploadFileService {
+  async uploadUserAvatar(
+    userRole: UserRole,
+    file: string
+  ): Promise<AxiosResponse<{ message: string }>> {
+    const response = await $api().post(
+      `/${PRIVATE_REQUESTS.UPLOAD_AVATAR}/${userRole}`,
+      {
+        avatar: file,
+      }
+    )
+
+    return response
+  }
+}
+
+const uploadFileService = new UploadFileService()
+
+export default uploadFileService
