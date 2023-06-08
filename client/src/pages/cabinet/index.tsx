0b6c3bac34ed4a6338f70708967fa18b0fc9673b
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-08 15:09:58 +0300

    client: removed modal from cabinet account page

--- client/src/pages/cabinet/index.tsx
+++ client/src/pages/cabinet/index.tsx
@@ -1,5 +1,5 @@
 //libs
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 //redux
 import { useAppSelector } from '@/redux/hooks'
@@ -9,14 +9,6 @@ import { fetchPaymentCard } from '@/redux/slices/payment-card'
 
 // material ui components
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Modal from '@mui/material/Modal'
-import Typography from '@mui/material/Typography'
-
-//custom components
-import { IconEnum } from '@/components/Generic/Icon/Icon.type'
-import Icon from '@/components/Generic/Icon'
-import CheckmarkIcon from '@/components/Generic/CheckmarkIcon'
 
 // layouts
 import CabinetLayout from '@/layouts/CabinetLayout'
@@ -32,7 +24,6 @@ import MainFieldsActivityBlock from '@/blocks/UserFieldsActivityBlock'
 function PageCabinet() {
   const userStore = useAppSelector((store) => store.user.user)
   const dispatch = useDispatch<any>()
-  const [modalOpen, setModalOpen] = useState<boolean>(false)
 
   useEffect(() => {
     if (userStore.role === 'teacher') {
@@ -40,16 +31,6 @@ function PageCabinet() {
       dispatch(fetchPaymentCard(userStore.role))
     }
   }, [dispatch])
-
-  useEffect(() => {
-    if (userStore.progress_account?.profile_progress === 100) {
-      setModalOpen(true)
-    }
-  }, [userStore.progress_account?.profile_progress])
-
-  function handleCloseModal() {
-    setModalOpen(false)
-  }
 
   return (
     <CabinetLayout>
@@ -79,40 +60,6 @@ function PageCabinet() {
           </Box>
         </>
       ) : null}
-      <Modal open={modalOpen} onClose={handleCloseModal}>
-        <Box className="modal-box" maxWidth={400}>
-          <Button
-            className="modal-box__button-close"
-            onClick={handleCloseModal}
-          >
-            <Icon
-              icon={IconEnum.CROSS_OUTLINE}
-              size={20}
-              color="#000000"
-              className="modal-box__button-close-icon"
-            />
-          </Button>
-          <Box className="modal-box__inner">
-            <Box textAlign="center" marginBottom={2}>
-              <CheckmarkIcon />
-            </Box>
-            <Typography variant="h4" className="ta-c" marginBottom={2}>
-              Congratulations you completed your account. <br /> Now all users
-              can see your profile
-            </Typography>
-            <Box textAlign="center" maxWidth={100} marginX="auto">
-              <Button
-                variant="contained"
-                size="small"
-                onClick={handleCloseModal}
-                fullWidth
-              >
-                Ok
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-      </Modal>
     </CabinetLayout>
   )
 }
