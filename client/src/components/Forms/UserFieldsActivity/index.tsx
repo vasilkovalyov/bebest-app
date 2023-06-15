74e6a48bb84353407c1de47770412cc89a8e2ad8
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-15 10:07:23 +0300

    client: show button add form for user fields activity component if there are no activities

--- client/src/components/Forms/UserFieldsActivity/index.tsx
+++ client/src/components/Forms/UserFieldsActivity/index.tsx
@@ -77,14 +77,15 @@ function UserFieldsActivityForm({
     index: -1,
   })
 
+  const defaultFields = fieldsActivityStore.length
+    ? defaultfieldsActivity.fields_activity
+    : []
+
   const { handleSubmit, register, setValue, getValues, control } =
     useForm<IUserFieldsActivityInfo>({
       mode: 'onSubmit',
       defaultValues: {
-        fields_activity: [
-          ...(fieldsActivityStore || []),
-          ...defaultfieldsActivity.fields_activity,
-        ],
+        fields_activity: [...(fieldsActivityStore || []), ...defaultFields],
       },
     })
 
@@ -211,6 +212,10 @@ function UserFieldsActivityForm({
     setCheckedSkills(checkedSubjectArr)
   }
 
+  function handleAddFormActivity() {
+    setValue('fields_activity', [defaultWorkExperience])
+  }
+
   useEffect(() => {
     loadSubjects()
   }, [])
@@ -218,169 +223,208 @@ function UserFieldsActivityForm({
   return (
     <Box>
       <form className="form">
-        <Box maxWidth={500}>
-          {fields.map(({ id, _id }, index) => (
-            <Box key={id}>
-              <Box marginBottom={2}>
-                <TextField
-                  {...register(`fields_activity.${index}.activity`)}
-                  id={`fields_activity.${index}.activity`}
-                  select
-                  type="text"
-                  label="Activity"
-                  variant="standard"
-                  className="form-field"
-                  placeholder="Activity"
-                  fullWidth
-                  onChange={(
-                    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-                  ) => {
-                    onHandleChangeActivity(e.target.value, index)
-                  }}
-                  InputLabelProps={{ shrink: true }}
-                  value={getValues().fields_activity[index].activity || ' '}
-                  defaultValue={
-                    fields.length - 1 !== index
-                      ? getValues().fields_activity[index].activity
-                      : [' ']
-                  }
-                  disabled={fields.length - 1 > index}
-                >
-                  <MenuItem disabled value=" ">
-                    Select activity
-                  </MenuItem>
-                  {subjects.length
-                    ? subjects.map(({ _id, category }) => (
-                        <MenuItem key={_id} value={category}>
-                          {category}
-                        </MenuItem>
-                      ))
-                    : null}
-                </TextField>
-              </Box>
-              <Box marginBottom={2}>
-                <InputLabel>Skills</InputLabel>
-                <Select
-                  {...register(`fields_activity.${index}.skills`)}
-                  id={`fields_activity-${index}-skills`}
-                  multiple
-                  value={
-                    checkedSkills.length &&
-                    checkedSkills[index] &&
-                    checkedSkills[index].subjects.length
-                      ? checkedSkills[index].subjects
-                      : ['']
-                  }
-                  defaultValue={['']}
-                  disabled={fields.length - 1 > index}
-                  onChange={(e: SelectChangeEvent<string[]>) =>
-                    onHandleChangeSkills(e, index)
-                  }
-                  renderValue={(selected) => {
-                    const updatedSelected = selected.filter(
-                      (item) => item !== ''
-                    )
-                    return updatedSelected.join(',')
-                  }}
-                >
-                  <MenuItem disabled value=" ">
-                    Select skills
-                  </MenuItem>
-
-                  {selectedSkills.length && selectedSkills[index]
-                    ? selectedSkills[index].subjects.map((item) => (
-                        <MenuItem key={item._id} value={item.subject}>
-                          <Checkbox
-                            checked={
-                              checkedSkills.length &&
-                              checkedSkills[index] &&
-                              checkedSkills[index].subjects
-                                ? checkedSkills[index].subjects.indexOf(
-                                    item.subject
-                                  ) > -1
-                                : false
-                            }
-                          />
-                          <ListItemText primary={item.subject} />
-                        </MenuItem>
-                      ))
-                    : null}
-                </Select>
-              </Box>
-              {fields.length - 1 > index ? (
-                <>
-                  <Button
-                    type="button"
-                    size="small"
-                    onClick={() => handleOpenModal(_id || '', index)}
+        {getValues().fields_activity.length ? (
+          <Box maxWidth={500}>
+            {fields.map(({ id, _id }, index) => (
+              <Box key={id}>
+                <Box marginBottom={2}>
+                  <TextField
+                    {...register(`fields_activity.${index}.activity`)}
+                    id={`fields_activity.${index}.activity`}
+                    select
+                    type="text"
+                    label="Activity"
+                    variant="standard"
+                    className="form-field"
+                    placeholder="Activity"
+                    fullWidth
+                    onChange={(
+                      e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+                    ) => {
+                      onHandleChangeActivity(e.target.value, index)
+                    }}
+                    InputLabelProps={{ shrink: true }}
+                    value={getValues().fields_activity[index].activity || ' '}
+                    defaultValue={
+                      fields.length - 1 !== index
+                        ? getValues().fields_activity[index].activity
+                        : [' ']
+                    }
+                    disabled={fields.length - 1 > index}
                   >
-                    <Box
-                      component="span"
-                      marginRight={1}
-                      display="inline-block"
-                      style={{
-                        verticalAlign: 'middle',
-                      }}
+                    <MenuItem disabled value=" ">
+                      Select activity
+                    </MenuItem>
+                    {subjects.length
+                      ? subjects.map(({ _id, category }) => (
+                          <MenuItem key={_id} value={category}>
+                            {category}
+                          </MenuItem>
+                        ))
+                      : null}
+                  </TextField>
+                </Box>
+                <Box marginBottom={2}>
+                  <InputLabel>Skills</InputLabel>
+                  <Select
+                    {...register(`fields_activity.${index}.skills`)}
+                    id={`fields_activity-${index}-skills`}
+                    multiple
+                    value={
+                      checkedSkills.length &&
+                      checkedSkills[index] &&
+                      checkedSkills[index].subjects.length
+                        ? checkedSkills[index].subjects
+                        : ['']
+                    }
+                    defaultValue={['']}
+                    disabled={fields.length - 1 > index}
+                    onChange={(e: SelectChangeEvent<string[]>) =>
+                      onHandleChangeSkills(e, index)
+                    }
+                    renderValue={(selected) => {
+                      const updatedSelected = selected.filter(
+                        (item) => item !== ''
+                      )
+                      return updatedSelected.join(',')
+                    }}
+                  >
+                    <MenuItem disabled value=" ">
+                      Select skills
+                    </MenuItem>
+
+                    {selectedSkills.length && selectedSkills[index]
+                      ? selectedSkills[index].subjects.map((item) => (
+                          <MenuItem key={item._id} value={item.subject}>
+                            <Checkbox
+                              checked={
+                                checkedSkills.length &&
+                                checkedSkills[index] &&
+                                checkedSkills[index].subjects
+                                  ? checkedSkills[index].subjects.indexOf(
+                                      item.subject
+                                    ) > -1
+                                  : false
+                              }
+                            />
+                            <ListItemText primary={item.subject} />
+                          </MenuItem>
+                        ))
+                      : null}
+                  </Select>
+                </Box>
+                {fields.length - 1 > index ? (
+                  <>
+                    <Button
+                      type="button"
+                      size="small"
+                      onClick={() => handleOpenModal(_id || '', index)}
                     >
-                      <Icon
-                        icon={IconEnum.BIN}
-                        color={colors.primary}
-                        size={16}
-                      />
+                      <Box
+                        component="span"
+                        marginRight={1}
+                        display="inline-block"
+                        style={{
+                          verticalAlign: 'middle',
+                        }}
+                      >
+                        <Icon
+                          icon={IconEnum.BIN}
+                          color={colors.primary}
+                          size={16}
+                        />
+                      </Box>
+                      <Box
+                        component="span"
+                        display="inline-block"
+                        style={{
+                          verticalAlign: 'middle',
+                          paddingTop: 4,
+                        }}
+                      >
+                        Remove
+                      </Box>
+                    </Button>
+                    <Box marginTop={1} marginBottom={4}>
+                      <Divider />
                     </Box>
-                    <Box
-                      component="span"
-                      display="inline-block"
-                      style={{
-                        verticalAlign: 'middle',
-                        paddingTop: 4,
-                      }}
-                    >
-                      Remove
-                    </Box>
-                  </Button>
-                  <Box marginTop={1} marginBottom={4}>
-                    <Divider />
-                  </Box>
-                </>
-              ) : null}
-            </Box>
-          ))}
-        </Box>
-        <Box display="flex" alignItems="center" marginBottom={3} maxWidth={400}>
-          <Stack direction="row" gap={2}>
-            <Button
-              type="submit"
-              variant="outlined"
-              size="small"
-              onClick={handleSubmit(handleAdd)}
+                  </>
+                ) : null}
+              </Box>
+            ))}
+            <Box
+              display="flex"
+              alignItems="center"
+              marginBottom={3}
+              maxWidth={400}
             >
-              <Box
-                component="span"
-                marginRight={1}
-                display="inline-block"
-                style={{
-                  verticalAlign: 'middle',
-                }}
-              >
-                <Icon icon={IconEnum.PLUS} color={colors.primary} size={16} />
-              </Box>
-              <Box
-                component="span"
-                display="inline-block"
-                style={{
-                  verticalAlign: 'middle',
-                  paddingTop: 2,
-                }}
-              >
-                Add activity
-              </Box>
-            </Button>
-            <Button variant="outlined" size="small" onClick={onHandleClose}>
-              Close
-            </Button>
-          </Stack>
-        </Box>
+              <Stack direction="row" gap={2}>
+                <Button
+                  type="submit"
+                  variant="outlined"
+                  size="small"
+                  onClick={handleSubmit(handleAdd)}
+                >
+                  <Box
+                    component="span"
+                    marginRight={1}
+                    display="inline-block"
+                    style={{
+                      verticalAlign: 'middle',
+                    }}
+                  >
+                    <Icon
+                      icon={IconEnum.PLUS}
+                      color={colors.primary}
+                      size={16}
+                    />
+                  </Box>
+                  <Box
+                    component="span"
+                    display="inline-block"
+                    style={{
+                      verticalAlign: 'middle',
+                      paddingTop: 2,
+                    }}
+                  >
+                    Add activity
+                  </Box>
+                </Button>
+                <Button variant="outlined" size="small" onClick={onHandleClose}>
+                  Close
+                </Button>
+              </Stack>
+            </Box>
+          </Box>
+        ) : (
+          <Button
+            type="submit"
+            variant="contained"
+            size="small"
+            onClick={handleAddFormActivity}
+          >
+            <Box
+              component="span"
+              marginRight={1}
+              display="inline-block"
+              style={{
+                verticalAlign: 'middle',
+              }}
+            >
+              <Icon icon={IconEnum.PLUS} color={colors.white} size={16} />
+            </Box>
+            <Box
+              component="span"
+              display="inline-block"
+              style={{
+                verticalAlign: 'middle',
+                paddingTop: 2,
+              }}
+            >
+              Add activity
+            </Box>
+          </Button>
+        )}
       </form>
       <Modal open={modalOpen} onClose={handleCloseModal}>
         <Box className="modal-box">
--- client/src/constants/colors.ts
+++ client/src/constants/colors.ts
@@ -1,5 +1,6 @@
 const colors = {
   primary: '#F04973',
+  white: '#ffffff',
   black: '#000000',
   blue: '#55ABFA',
 }
