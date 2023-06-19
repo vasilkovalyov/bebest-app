8764d1cce33c0984bcb19c64ce537e7eb6ed3ff4
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-19 19:14:49 +0300

    client: create services for company

---
+++ client/src/services/company.ts
@@ -0,0 +1,76 @@
+import $api from '@/utils/ajax'
+import { AxiosResponse } from 'axios'
+import { PRIVATE_REQUESTS } from '@/constants/api-requests'
+import { PUBLIC_REQUESTS } from '@/constants/api-requests'
+import { IRegistrationResponse } from '@/interfaces/common'
+import { IRegistrationCompany } from '../components/Forms/Registration/RegistrationCompany/RegistrationCompany.type'
+import { IAuthUserInfo } from '@/redux/slices/auth'
+
+export type UserAccountInfoEditType = Omit<
+  IAuthUserInfo,
+  'role' | '_id' | 'progress_account' | 'video'
+>
+
+class CompanyService {
+  constructor() {}
+
+  async getUserInfo(
+    token?: string | undefined
+  ): Promise<AxiosResponse<IAuthUserInfo>> {
+    const response = await $api(token).get(
+      `/${PRIVATE_REQUESTS.USER_INFO}/company`
+    )
+    return response
+  }
+
+  async registration(
+    props: IRegistrationCompany
+  ): Promise<AxiosResponse<IRegistrationResponse>> {
+    const response = await $api().post(
+      `/${PUBLIC_REQUESTS.REGISTRATION_COMPANY}`,
+      {
+        ...props,
+      }
+    )
+
+    return response
+  }
+
+  async deleteUser(): Promise<AxiosResponse<{ data: boolean }>> {
+    const response = await $api().delete(
+      `/${PRIVATE_REQUESTS.USER_DELETE}/company`
+    )
+
+    return response
+  }
+
+  async updateUserAccountInfo(
+    props: UserAccountInfoEditType
+  ): Promise<AxiosResponse<IAuthUserInfo>> {
+    const response = await $api().post(
+      `/${PRIVATE_REQUESTS.USER_INFO}/company`,
+      {
+        ...props,
+      }
+    )
+
+    return response
+  }
+
+  async changePassword(
+    password: string
+  ): Promise<AxiosResponse<{ message: string }>> {
+    const response = await $api().post(
+      `/${PRIVATE_REQUESTS.UPDATE_PASSWORD}/company`,
+      {
+        password: password,
+      }
+    )
+
+    return response
+  }
+}
+
+const companyService = new CompanyService()
+
+export default companyService
