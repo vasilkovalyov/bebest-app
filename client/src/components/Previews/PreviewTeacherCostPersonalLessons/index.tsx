705a519e3e1e75ebd1781545d9d8dc1be8b311f9
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-21 16:39:17 +0300

    client: refactoring display data in preview teacher cost personal lessons

--- client/src/components/Previews/PreviewTeacherCostPersonalLessons/index.tsx
+++ client/src/components/Previews/PreviewTeacherCostPersonalLessons/index.tsx
@@ -12,37 +12,36 @@ import { getTrialLessonResult } from './PreviewTeacherCostPersonalLessons.utils'
 function PreviewTeacherCostPersonalLessons({
   data,
 }: IPreviewTeacherCostPersonalLessonsProps) {
+  if (!data || !Object.keys(data || {}).length) {
+    return (
+      <Typography variant="body1">
+        No data. Click on Edit button to add information.
+      </Typography>
+    )
+  }
   return (
-    <>
-      {data ? (
-        <Box>
-          <PreviewInfo
-            category="Private lessons"
-            heading={data.duration + ' ' + 'hour'}
-            values={[data.is_free ? 'Free' : data.price + ' ' + '$']}
-            marginBottom={4}
-          />
-          {data.use_trial ? (
-            <PreviewInfo
-              category="Trial lessons"
-              heading={data.trial_duration + ' ' + 'hour'}
-              values={[
-                getTrialLessonResult(
-                  data.use_trial,
-                  data.is_trial_free,
-                  data.trial_price + ' ' + '$'
-                ),
-              ]}
-              marginBottom={0}
-            />
-          ) : null}
-        </Box>
-      ) : (
-        <Typography variant="body1">
-          No data. Click on Edit button to add information.
-        </Typography>
-      )}
-    </>
+    <Box>
+      <PreviewInfo
+        category="Private lessons"
+        heading={data.duration + ' ' + 'hour'}
+        values={[data.is_free ? 'Free' : data.price + ' ' + '$']}
+        marginBottom={4}
+      />
+      {data.use_trial ? (
+        <PreviewInfo
+          category="Trial lessons"
+          heading={data.trial_duration + ' ' + 'hour'}
+          values={[
+            getTrialLessonResult(
+              data.use_trial,
+              data.is_trial_free,
+              data.trial_price + ' ' + '$'
+            ),
+          ]}
+          marginBottom={0}
+        />
+      ) : null}
+    </Box>
   )
 }
 
