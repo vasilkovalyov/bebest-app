da9f5032c0127e52e128337a437436f4d6f76a20
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-22 18:16:32 +0300

    client: add next bundle analyzer

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
-module.exports = {
+const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles/scss')],
   },
@@ -16,3 +16,8 @@ module.exports = {
     apiUrl: process.env.API_URL,
   },
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer(nextConfig)
