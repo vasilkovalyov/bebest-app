311e4ab26f70ddb7f7d574c2dd7a2162059f470e
vasilkovalyov <vasilkovalyov@gmail.com>
2023-06-29 21:50:14 +0300

    client: updates

--- client/src/pages/cabinet/lessons/index.tsx
+++ client/src/pages/cabinet/lessons/index.tsx
@@ -1,29 +1,20 @@
-// libs
-import Link from 'next/link'
-import { ChangeEvent, useEffect, useState } from 'react'
+//redux
+import { useAppSelector } from '@/redux/hooks'
 
 // material ui components
-import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import Modal from '@mui/material/Modal'
 import Box from '@mui/material/Box'
-import Stack from '@mui/material/Stack'
 
 //custom components
 import ContainerWithShadow from '@/components/Generic/ContainerWithShadow'
-import Icon from '@/components/Generic/Icon'
-import { IconEnum } from '@/types/icons'
-import LessonTypeBox from '@/components/LessonTypeBox'
 
 // layouts
 import CabinetLayout from '@/layouts/CabinetLayout'
 
-// other utils
-import colors from '@/constants/colors'
-import { LessonType } from '@/types/lessons'
 import TeacherLessonsBlock from '@/blocks/TeacherLessonsBlock'
 
 export default function PageLessons() {
+  const user = useAppSelector((store) => store.user.user)
   return (
     <CabinetLayout currentPageTitle="Lessons">
       <Box marginBottom={4}>
@@ -37,7 +28,7 @@ export default function PageLessons() {
           </Typography>
         </ContainerWithShadow>
       </Box>
-      <TeacherLessonsBlock />
+      {user.role === 'teacher' ? <TeacherLessonsBlock /> : null}
     </CabinetLayout>
   )
 }
